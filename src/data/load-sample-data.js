0afd068084e6a770f1db36fdea295e56ed208e10
José Nunes <josenunes34@gmail.com>
2019-05-16 18:02:33 +0100

    Fix script message
    WIDY-134

--- src/data/load-sample-data.js
+++ src/data/load-sample-data.js
@@ -36,7 +36,7 @@ async function loadData() {
     process.exit();
   } catch (e) {
     console.log(
-      '\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n',
+      '\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run delete\n\n\n',
     );
     console.log(e);
     process.exit();
